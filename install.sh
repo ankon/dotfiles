074615660cc3a7f4566f094d8575c7401c65992f
Andreas Kohn <andreas.kohn@gmail.com>
2022-03-31 12:50:41 +0200

    Update the installer to have macOS/Linux specific parts
    While there: Add configuration for using pinentry-mac on macOS for GPG

--- install.sh
+++ install.sh
@@ -3,8 +3,30 @@
 echo "WARNING: Incomplete, and will likely destroy existing configuration" >&2
 exit 1
 
-# Install custom mime types
-for config in .local/share/mime/*.xml; do
-    xdg-mime install "${config}"
-done
-update-mime-database ~/.local/share/mime/
+install_linux() {
+	# Install custom mime types
+	for config in .local/share/mime/*.xml; do
+	    xdg-mime install "${config}"
+	done
+	update-mime-database ~/.local/share/mime/
+}
+
+install_darwin() {
+	# Install keybindings
+	mkdir -p ~/Library/KeyBindings
+	ln -sf $PWD/Library/KeyBindings/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
+	# Install GPG config
+	mkdir -p ~/.gnupg
+	ln -sf $PWD/.gnupg.Darwin/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+}
+
+case "$(uname)" in
+	Darwin)
+		install_darwin
+		;;
+	Linux)
+		install_linux
+		;;
+	*)
+		;;
+esac
