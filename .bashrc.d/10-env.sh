3091924e42c94992a72e2f51a22c4b928a38958a
Andreas Kohn <andreas.kohn@gmail.com>
2025-02-12 10:09:33 +0100

    Export the environment variables from the env script
    This fixes the problem that electron apps use xwayland, and possibly also the problem that
    Firefox opens URLs in some oddball cases such as clicking on a downloaded file in Edge.

--- .bashrc.d/10-env.sh
+++ .bashrc.d/10-env.sh
@@ -3,6 +3,7 @@
 # Electron 28+: Use wayland if possible
 # See https://github.com/electron/electron/blob/main/docs/api/environment-variables.md#electron_ozone_platform_hint-linux
 ELECTRON_OZONE_PLATFORM_HINT=auto
+export ELECTRON_OZONE_PLATFORM_HINT
 
 # Default browser
 # This should be aligned with the xdg settings:
@@ -10,3 +11,4 @@ XDG_DEFAULT_BROWSER=$(xdg-settings get default-web-browser 2>/dev/null || echo "
 if [ -n "${XDG_DEFAULT_BROWSER}" ]; then
 	BROWSER=${XDG_DEFAULT_BROWSER}
 fi
+export XDG_DEFAULT_BROWSER BROWSER
