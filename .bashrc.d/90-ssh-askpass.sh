8cc3d48d94da0a63b16a4f1b1f01ae7c1fa0f02f
Andreas Kohn <andreas.kohn@gmail.com>
2025-05-15 13:28:09 +0200

    Avoid producing output when checking the SSH_ASKPASS environment variable
    The output confuses scp when it tries to open a session: It expects non-interactive sessions to not
    have any output.
    
    Note that the logic here works, but is incomplete: When there is no UI available we cannot use `gnome-ssh-askpass`, and instead
    we should add a wrapper script here that uses a CLI fallback.

--- .bashrc.d/90-ssh-askpass.sh
+++ .bashrc.d/90-ssh-askpass.sh
@@ -3,7 +3,7 @@
 # kde-settings gets installed by a bunch of things (such as libreoffice and java) and
 # sets `SSH_ASKPASS` to /usr/bin/ksshaskpass -- but that's not depended upon.
 if [ -n "${SSH_ASKPASS}" ]; then
-	if command -v "${SSH_ASKPASS}"; then
+	if command -v "${SSH_ASKPASS}" >/dev/null 2>&1; then
 		# All good: The value seems to be pointing to an executable.
 		:
 	else
@@ -12,7 +12,7 @@ if [ -n "${SSH_ASKPASS}" ]; then
 		# default choice.
 		SSH_ASKPASS=
 		for maybe_askpass in /usr/libexec/openssh/ssh-askpass /usr/libexec/gnome-ssh-askpass; do
-			if command -v "${maybe_askpass}"; then
+			if command -v "${maybe_askpass}" >/dev/null 2>&1; then
 				SSH_ASKPASS=${makybe_askpass}
 				break
 			fi
