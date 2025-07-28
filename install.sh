b8092ed34197e42eabee69966effbc2c7702e3d4
Andreas Kohn <andreas.kohn@gmail.com>
2025-07-28 11:05:32 +0200

    Fix the condition for installing docker-compose

--- install.sh
+++ install.sh
@@ -185,7 +185,7 @@ EOF
 
 	# Install docker-compose
 	DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-	if test -x "${DOCKER_CONFIG}/cli-plugins/docker-compose"; then
+	if ! test -x "${DOCKER_CONFIG}/cli-plugins/docker-compose"; then
 		mkdir -p $DOCKER_CONFIG/cli-plugins
 		curl -SL https://github.com/docker/compose/releases/download/v2.39.1/docker-compose-linux-$(uname -m) -o "${DOCKER_CONFIG}/cli-plugins/docker-compose"
 		chmod +x "${DOCKER_CONFIG}/cli-plugins/docker-compose"
