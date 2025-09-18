07233b3a834420175431d6b3f06b5c7d46220257
Andreas Kohn <andreas.kohn@gmail.com>
2025-09-18 11:37:34 +0200

    Add a note why we want `vim` on macOS

--- .bashrc.d/vim.sh
+++ .bashrc.d/vim.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Prefer vim, if available.
+# See also https://stackoverflow.com/a/69735583/196315: This fixes the issue on macOS that `vi` would exit with 1 if
+# any command in the session was invalid.
 _vim=`which vim`
 if [ -n "${_vim}" ] && [ -x "${_vim}" ]; then
 	alias vi=${_vim}
