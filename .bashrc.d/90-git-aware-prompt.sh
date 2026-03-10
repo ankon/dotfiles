261d296470bfc8b95ca6e6fc43590dea069fc7ca
Andreas Kohn <andreas.kohn@gmail.com>
2026-03-10 22:19:19 +0100

    Use powerline-go instead of git-aware-prompt
    This is still "experimental", and the git-aware-prompt is only neutralized using `if false` for now.

---
+++ .bashrc.d/90-git-aware-prompt.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if false; then
+	# Configure a reasonably sane prompt
+	# - Inject git information
+	export GITAWAREPROMPT=$DOTFILES_HOME/third-party/git-aware-prompt
+
+	if [ -f $GITAWAREPROMPT/main.sh ]; then
+		. "${GITAWAREPROMPT}/colors.sh"
+
+		# We only need the side-effects of the defined functions, not the prompt manipulation itself.
+		PROMPT_COMMAND_save=$PROMPT_COMMAND
+		. "${GITAWAREPROMPT}/prompt.sh"
+		PROMPT_COMMAND=$PROMPT_COMMAND_save
+	fi
+	PROMPT_COMMAND=${PROMPT_COMMAND:+$PROMPT_COMMAND;}"find_git_branch;find_git_dirty"
+
+	PS1="\$git_user_warning[\u@\h \W\$git_branch\$git_dirty]\$ "
+	DOTFILES_PS1="$PS1"
+	case $TERM in
+	xterm-*|rxvt-*|foot|foot-*)
+		# Export the working directory into the window title
+		DOTFILES_PS1="$DOTFILES_PS1\[\033]0;🖵 \W\007\]"
+		;;
+	*)
+		;;
+	esac
+	export DOTFILES_PS1
+	PS1=$DOTFILES_PS1
+	export PS1
+fi
+
---
+++ .bashrc.d/90-powerline.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+function _update_ps1() {
+    PS1="$(powerline-go -error $? -jobs $(jobs -p | wc -l) -condensed -cwd-max-depth 2)"
+
+    # Uncomment the following line to automatically clear errors after showing
+    # them once. This not only clears the error for powerline-go, but also for
+    # everything else you run in that shell. Don't enable this if you're not
+    # sure this is what you want.
+
+    #set "?"
+}
+
+if [ "$TERM" != "linux" ] && command -v powerline-go >/dev/null 2>&1; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
+
